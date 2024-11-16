5e004aac65bbc7204496ce62faf36c3bb3357445
Anu Aniyammal S <anuaniyammal@gmail.com>
2024-11-16 22:08:50 +0530

    Update main.tf

--- Windows VM terraform script/main.tf
+++ Windows VM terraform script/main.tf
@@ -1,1 +1,97 @@
 
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.8.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {} 
+ client_id       = "e723b617-bace-4aff-bd44-7e3e20cb668d"
+ client_secret   = "1SR8Q~K9BoEGQJWOwOPkUrBkNiehx7FPncAcMdas"
+ tenant_id       = "f9ed46e5-472f-4cdc-819a-af8a2126d851"
+ subscription_id = "37de05e2-82d1-4127-b298-36b6f83c3fa5"
+}
+
+#create resource group
+resource "azurerm_resource_group" "rg" {
+    name     = "${var.rgname}"
+    location = "${var.location}"
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "${var.prefix}-10"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = "${azurerm_resource_group.rg.location}"
+  address_space       = ["${var.vnet_cidr_prefix}"]
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  address_prefixes     = ["${var.subnet1_cidr_prefix}"]
+}
+
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "${var.prefix}-nsg1"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = "${azurerm_resource_group.rg.location}"
+}
+
+# NOTE: this allows RDP from any network
+resource "azurerm_network_security_rule" "rdp" {
+  name                        = "rdp"
+  resource_group_name         = "${azurerm_resource_group.rg.name}"
+  network_security_group_name = "${azurerm_network_security_group.nsg1.name}"
+  priority                    = 102
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_subnet_assoc" {
+  subnet_id                 = azurerm_subnet.subnet1.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
+resource "azurerm_network_interface" "nic1" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                            = "${var.prefix}-vmt01"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_B1s"
+  admin_username                  = "adminuser"
+  admin_password                  = "P@ssw0rd1234!"
+  network_interface_ids = [ azurerm_network_interface.nic1.id ]
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
